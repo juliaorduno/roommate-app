3cf623394233b4ca35154480676a1fd3ecdde89d
MikeSagnelli <ing.sagnelli@gmail.com>
2019-03-06 08:05:11 -0600

    feat: add signup component

---
+++ components/Auth/SignUp/styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ButtonContainer = styled.div`
+  margin-top: 29px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default ButtonContainer;
