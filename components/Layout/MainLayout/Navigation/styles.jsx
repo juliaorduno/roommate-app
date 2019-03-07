b26ea24d3a7be5211b4730a5d3dd79a1f14b4563
MikeSagnelli <ing.sagnelli@gmail.com>
2019-03-07 14:19:26 -0600

    feat(layout): add navigation to main layout

---
+++ components/Layout/MainLayout/Navigation/styles.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { colors } from '../../../../static/styleConstants';
+
+export const NavBar = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  width: 100%;
+  background-color: ${colors.whiteColor};
+  box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.1);
+`;
+
+export const LogoImg = styled.img`
+  width: 134px;
+  position: absolute;
+  left: 45px;
+  top: 18px;
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 683px;
+`;
+
+export const LinkContainer = styled.div`
+  height: 100%;
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const LinkItem = styled.a`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+
+  &.active {
+    color: ${colors.primaryColor};
+  }
+
+  &:not(.active) {
+    color: ${colors.blackColor};
+  }
+`;
