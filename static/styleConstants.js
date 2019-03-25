0ad33fb567c4a77c4719864e2ab5d80138c217eb
MikeSagnelli <ing.sagnelli@gmail.com>
2019-03-24 21:26:52 -0600

    chore: add card reusable component

--- static/styleConstants.js
+++ static/styleConstants.js
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 export const buttonsBorderRadius = '30px';
 
 export const colors = {
@@ -20,3 +22,9 @@ export const colors = {
 export const defaultBorderRadius = '10px';
 
 export const defaultBoxShadow = '0 2px 6px 2px rgba(0,0,0,0.1)';
+
+export const CardContainer = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.1);
+`;
