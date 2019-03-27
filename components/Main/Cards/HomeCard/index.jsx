4ebe0ed4b010cb0ea7d1ef0d40594175e05a0c5f
MikeSagnelli <ing.sagnelli@gmail.com>
2019-03-27 01:32:34 -0600

    feat: create multi type card caller

---
+++ components/Main/Cards/HomeCard/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ShoppingCard from './ShoppingCard';
+import AnnouncementCard from './AnnouncementCard';
+
+const HomeCard = ({ data }) => {
+  switch (data.type) {
+    case 'shopping':
+      return (
+        <ShoppingCard
+          avatar={data.avatar ? data.avatar : ''}
+          userName={data.userName}
+          date={data.date}
+          item={data.item}
+        />
+      );
+    case 'announcement':
+      return (
+        <AnnouncementCard
+          avatar={data.avatar ? data.avatar : ''}
+          userName={data.userName}
+          date={data.date}
+          announcement={data.announcement}
+        />
+      );
+    default:
+      return null;
+  }
+};
+
+HomeCard.displayName = 'HomeCard';
+
+HomeCard.propTypes = {
+  data: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    item: PropTypes.string,
+    announcement: PropTypes.string,
+    avatar: PropTypes.string
+  }).isRequired
+};
+
+export default HomeCard;
