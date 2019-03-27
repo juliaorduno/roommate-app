06afde975e71a201d16ad551548002592489a711
MikeSagnelli <ing.sagnelli@gmail.com>
2019-03-27 01:32:13 -0600

    feat: reuse main card into shopping main card

---
+++ components/Main/Cards/HomeCard/ShoppingCard/Title.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BoldText, Highlight, NormalText } from '../styles';
+
+const Title = ({ userName, item }) => (
+  <React.Fragment>
+    <BoldText>{userName}</BoldText>
+    <NormalText>&ensp;added&ensp;</NormalText>
+    <BoldText>{item}</BoldText>
+    <NormalText>&ensp;to the&ensp;</NormalText>
+    <Highlight>shopping list.</Highlight>
+  </React.Fragment>
+);
+
+Title.propTypes = {
+  userName: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired
+};
+
+export default Title;
